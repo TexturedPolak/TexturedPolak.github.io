be51b61bec14821da0b3f59169fded64ff336acf
TexturedPolak <rafal200725@gmail.com>
2022-09-23 18:08:15 +0000

    content

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ document.getElementById("js-alert").classList.add("is-hidden");
 const hash = window.location.hash.replace("#","") || "home";
 //Rozdziela podstronę od podstrony podstrony
 const readyHash = hash.split("/");
+const home = "home"
 //Dla wszystkich Div'ów z klasą page dodaję klasę is-hidden ukrywającą niepotrzebną zawartość
 document.querySelectorAll(".page").forEach(function($e){
 	$e.classList.add("is-hidden")
 });
 //Sprawdzanie czy to podstrona czy podstrona podstrony i odkrywanie porządanej zawartości
 //Podstrona podstrony
+if (readyHash[0] == "prev"){
+
+document.getElementById(home).classList.remove("is-hidden")
+
+}
+else{
 if (readyHash.length == 2){
 	//Odkrywanie porządanej zawartości
 	document.getElementById(readyHash[1]).classList.remove("is-hidden")
@@ -19,14 +26,16 @@ if (readyHash.length == 2){
 if (readyHash.length == 1){
 	//Odkrywanie porządanej zawartości
 	document.getElementById(readyHash[0]).classList.remove("is-hidden")
-};
+};};
 //Podmienianie zawartości strony w locie
 //Oczekiwanie aż hash się zmieni
 window.addEventListener("hashchange", function(){
 	//Usunięcie znaku "#" i zastąpienie pustego #, #home
+	prevHash = null
+	const home = "home"
 	const hash = window.location.hash.replace("#","") || "home"
-	//Rozdziela podstronę od podstrony podstrony
 	const readyHash = hash.split("/")
+	//Rozdziela podstronę od podstrony podstrony
 	//Dla wszystkich Div'ów z klasą page dodaję klasę is-hidden ukrywającą niepotrzebną zawartość
 	document.querySelectorAll(".page").forEach(function($e){
 		$e.classList.add("is-hidden")
@@ -36,10 +45,10 @@ window.addEventListener("hashchange", function(){
 	if (readyHash.length == 2){
 		//Odkrywanie porządanej zawartości
 		document.getElementById(readyHash[1]).classList.remove("is-hidden")
-	}
+	};
 	//Podstrona
 	if (readyHash.length == 1){
 		//Odkrywanie porządanej zawartości
 		document.getElementById(readyHash[0]).classList.remove("is-hidden")
-	}
+	};
 });
